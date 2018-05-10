78f725878ff2fb4bee37c8e1a77b9c8fa26d6e2f
Calvin <cd-public@users.noreply.github.com>
2018-05-10 17:08:16 -0400

    Create remove_config.bat

---
+++ UNC-CH/Research/WORKSPACE/remove_config.bat
@@ -0,0 +1,4 @@
+setenv PATH ${PATH}:"/opt/cadence/installs/INCISIVE152/tools.lnx86/bin/64bit"
+setenv PATH ${PATH}:"/opt/cadence/installs/INCISIVE152/tools.lnx86/bin"
+setenv CDS_LIC_FILE "5280@cadence.cs.unc.edu"
+setenv LM_LICENSE_FILE "/opt/cadence/installs/LCU/License_30187_1866daf8e644_6_29_2017.txt"
