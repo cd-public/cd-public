3a28c954bba1bf6b76fc83efc95bdbc1a2e2e88e
Calvin <cd-public@users.noreply.github.com>
2018-05-21 13:19:12 -0400

    Create sr.py

---
+++ UNC-CH/Teaching/5_21/sr.py
@@ -0,0 +1,12 @@
+## Name: String Reverse
+## In:  to_reverse:  a string to be reversed
+## Out: the reversed string
+## Example:
+## >>> sr.string_reverse("abc")
+## "cba"
+## >>>
+def string_reverse(to_reverse):
+	temp = list(to_reverse)
+	for i in range(len(to_reverse)):
+		temp[len(to_reverse) - 1 - i] = to_reverse[i]
+	return "".join(temp)
