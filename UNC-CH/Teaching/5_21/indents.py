c75849b81e85e1ffb3c5d25335223af15c780e16
Calvin <cd-public@users.noreply.github.com>
2018-05-21 13:31:48 -0400

    Update indents.py

--- UNC-CH/Teaching/5_21/indents.py
+++ UNC-CH/Teaching/5_21/indents.py
@@ -1,7 +1,8 @@
 # indents.py: an example program showing the importance of consistent indentation
+# runnable version - indents in program are correct, but gives a demonstration
 
 def indents():
 	print("This line has a tab:    \"\t\"")
-	print("This line has 4 spaces: \"\s\s\s\s\"")
+	print("This line has 4 spaces: \"    "")
 
 indents()
