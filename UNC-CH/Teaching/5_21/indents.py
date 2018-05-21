35e0f15191c33f4e059bad76c49d2449b8ee682b
Calvin <cd-public@users.noreply.github.com>
2018-05-21 13:28:31 -0400

    Create indents.py

---
+++ UNC-CH/Teaching/5_21/indents.py
@@ -0,0 +1,5 @@
+# indents.py: an example program showing the importance of consistent indentation
+
+def indents():
+	print("This line has a tab:    "\t")
+    print("This line has 4 spaces: "\s\s\s\s")
