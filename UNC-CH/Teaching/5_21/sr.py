9ccf981a39a29ea7d67b3df728bd8136dd064843
Calvin <cd-public@users.noreply.github.com>
2018-05-21 13:19:38 -0400

    Update sr.py

--- UNC-CH/Teaching/5_21/sr.py
+++ UNC-CH/Teaching/5_21/sr.py
@@ -1,6 +1,6 @@
 ## Name: String Reverse
-## In:  to_reverse:  a string to be reversed
-## Out: the reversed string
+## In:   to_reverse:  a string to be reversed
+## Out:  the reversed string
 ## Example:
 ## >>> sr.string_reverse("abc")
 ## "cba"
