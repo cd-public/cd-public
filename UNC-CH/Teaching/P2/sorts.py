17414e4fe1fc249d695edbae510738c779d13136
Calvin <cd-public@users.noreply.github.com>
2018-06-11 15:35:23 -0400

    Update sorts.py

--- UNC-CH/Teaching/P2/sorts.py
+++ UNC-CH/Teaching/P2/sorts.py
@@ -33,6 +33,7 @@ import random, time
 # 2,1 <size = 2 (1 * 2), unsorted>
 # 1,2,3,4 <size = 4 (1 * 2 * 2), presorted>
 # 2,3,1,4 <size = 4 (1 * 2 * 2), unsorted>
+# these "size=1" just gives default arguments, similar to stuff in A2
 def gen_tests(size=1,gf=2,sizes=3,fname="tests.csv"):
   return # your code here
     
@@ -75,4 +76,52 @@ def merge(list1,list2):
 def merge_sort(list):
   return # your code here
 
+##
+## Graph
+##
 
+# timed sort - takes a list of integer (ie [3,4,1,2]) and a sorting function and returns a time it took to run the sort
+# example:  timed_sort(bst_sort, list) returns some time (ie 0.0060004196) in seconds
+def timed_sort(sort_fun, list):
+  start = time.time()
+  sort_fun(list)
+  return time.time() - start
+  
+# time the 3 different sorts on all the data sets in inname and save the times as a csv to outname
+# something like:
+# size, bst, sl, 3
+# 10000, 0.06, 0.15, 0.04
+# 10000000, 0.51, 1.07, 0.35
+def gen_data(inname="tests.csv", outname="times.csv")
+  return # your code here
+
+# graph the csv a la the example and A2
+def gen_graph(data="times.csv")
+  return # your code here
+
+##
+## Imagery
+##
+
+# make bst image - makes a picture that looks like a bst
+def make_bst_image():
+  return # maybe you want to take a bst as input or whatever
+
+# make sl image - makes a picture that looks like a bst
+def make_sl_image():
+  return # I drew out lists a bit in class.  Maybe something like that?
+
+def make_3_image():
+  return # whatever your third sort is, a picture is worth a thousand words
+
+# if you want to combine the 3 images (I don't care as long as you make something)
+# takes 3 images as .png files
+def image_combine(img1,img2,img3):
+  img1 = Image.open(img1 + ".png")
+  img2 = Image.open(img2 + ".png")
+  img3 = Image.open(img3 + ".png")
+	result = Image.new("RGB", (100, 300)) # gonna assume 3 100x100 images - if they
+  result.paste(img1,(0,0,100,100))
+	result.paste(img2,(0,100,100,200))
+	result.paste(img2,(0,200,100,300))
+  result.save("image.png")
